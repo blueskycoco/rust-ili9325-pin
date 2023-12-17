22b6a9593ded9f817a47c65e159621c04ac480a3
Dillon Min <dillon.minfei@gmail.com>
2023-12-17 10:21:16 +0800

    format code style
    Signed-off-by: Dillon Min <dillon.minfei@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -20,28 +20,28 @@ pub struct Interface {
 }
 
 impl Interface {
-	pub fn new() -> Interface {
-		Interface {
-		}
-	}
+    pub fn new() -> Interface {
+        Interface {
+        }
+    }
 }
 
 impl ili9325_lcd::Interface for Interface {
     fn write_command(&self, command: u16) {
-		write_command(command);
-	}
+        write_command(command);
+    }
 
-	fn write_data(&self, data: u16) {
-	    write_data(data);
-	}
+    fn write_data(&self, data: u16) {
+        write_data(data);
+    }
 
     fn read_data(&self, data: &mut u16) {
         *data = read_data();
-	}
+    }
 
-	fn reset(&self) {
+    fn reset(&self) {
 	    reset();
-	}
+    }
 }
 
 pub type Controller = ili9325_lcd::Controller<Interface>;
